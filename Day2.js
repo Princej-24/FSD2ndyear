ff633968c05b6699c82408136a4f8c519c699a94
Prince <princejaiswalpj251105@gmail.com>
2026-07-23 14:42:02 +0530

    day 2

---
+++ Day2.js
@@ -0,0 +1,28 @@
+// // callback function
+// function hello(n1, n2, callback) {
+//     console.log("Hello World");
+//     callback();
+// }
+// let a = 10;
+// let b = 20;
+// console.log(hello(a, b, sayHi));
+// console.log(hello(a, b, sayHello));
+// console.log((hello(a, b, function demo() {
+//     console.log("callback is calling");
+// })));
+
+// function sayHi() {
+//     console.log("callback function");
+// }
+// sayHi();
+
+// function sayHello() {
+//     console.log("This is 2nd callback function");
+// }
+// sayHello();
+
+
+
+// promises
+
+
