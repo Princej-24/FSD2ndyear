bf67824932f69def08522981219a8dfc752f064c
Prince <princejaiswalpj251105@gmail.com>
2026-07-27 16:30:15 +0530

    Day 3

--- Day2.js
+++ Day2.js
@@ -22,14 +22,57 @@
 // sayHello();
 
 
+// callback function
+function hello(n1, n2, callback) {
+    console.log(n1 + n2);
+    callback();
+}
 
-// promises
-const promiseOne = new Promise((resolve, reject) => {
-    console.log("Promise is pending");
-    resolve("Promise resolved");
+let a = 10;
+let b = 20;
+hello(a, b, sayHi);
+hello(a, b, sayHello);
+hello(a, b, function demo() {
+    console.log("callback is calling");
 });
-promiseOne.then((data) => {
-    console.log("Promise resolved with data:", data);
+
+function sayHi() {
+    console.log("callback function");
+}
+sayHi();
+
+function sayHello() {
+    console.log("This is 2nd callback function");
+}
+sayHello();
+
+
+// Promises
+const promiseOne = new Promise((resolve, reject) => {
+    console.log("Successfully data passed......");
+    resolve("resolve promised");
+});
+promiseOne.then((result) => {
+    console.log(result);
 }).catch((error) => {
-    console.error("Promise rejected with error:", error);
+    console.log(error);
+}); 
+
+
+new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        let msg = "true";
+        if(msg) {
+            resolve("user : Asynchronous task");
+        } else {
+            reject("Error : Undefined data...");
+        }
+        console.log("synchronous task....");
+        resolve();
+    },1000)
+}).then(function(result) {
+    console.log("asynchronous task 2");
+
+}).catch(function(error) {
+    console.log(error);
 });
