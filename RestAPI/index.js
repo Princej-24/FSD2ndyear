8edec5647e716d8a26a571b509c96e571e608a10
Prince <princejaiswalpj251105@gmail.com>
2026-09-09 09:22:37 +0530

    Rest API

--- RestAPI/index.js
+++ RestAPI/index.js
@@ -1,33 +1,39 @@
 import express from "express";
-const app = express();
-app.use(express.json());
-let users = [{
-    id:1,
-    name:"A",
-    email:"a@abes.ac.in"
-},
-{
-    id:2,
-    name:"B",
-    email:"b@abes.ac.in"
-}];
 
-// GET request 
-app.get("/users",(req, res) => {
+const app = express();
+
+app.use(express.json());
+
+let users = [
+    {
+        id: 1,
+        name: "A",
+        email: "a@abes.ac.in"
+    },
+    {
+        id: 2,
+        name: "B",
+        email: "b@abes.ac.in"
+    }
+];
+
+// GET request
+app.get("/users", (req, res) => {
     res.json(users);
 });
 
-// POST request to create a new server
-app.post("/users",(req, res) => {
+// POST request to create a new user
+app.post("/users", (req, res) => {
     const user = {
-        id:users.length+1,
-        name:req.body.name,
-        email:req.body.email
+        id: users.length + 1,
+        name: req.body.name,
+        email: req.body.email
     };
+
     users.push(user);
     res.json(user);
 });
 
-app.listen(8000,() => {
-    console.log("Server is running on http://localhost:8000")
-})
+app.listen(8000, () => {
+    console.log("Server is running on http://localhost:8000");
+});
